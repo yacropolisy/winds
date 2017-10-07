f63e2a6e98c90ec8429109f8e1aa0339a3689634
vagrant <vagrant@localhost.localdomain>
2017-10-07 11:47:50 +0900

    robotex installed

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-
+gem 'robotex'
 gem 'rails_12factor', group: :production
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @msg = 'hello!'
+
+    require 'open-uri'
+    require 'nokogiri'
+    # スクレイピング先のURL
+    url = 'http://weather-gpv.info/'
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+   # htmlをパース(解析)してオブジェクトを作成
+   doc = Nokogiri::HTML.parse(html, nil, charset)
+   @title = doc.title
+   @img = doc.xpath('html/frameset/frame/html/body/div/span/img')
   end
 end
