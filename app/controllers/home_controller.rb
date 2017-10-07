0b71fedc8be7075bee89ddab1d50c39df88a6627
vagrant <vagrant@localhost.localdomain>
2017-10-07 19:13:34 +0900

    save articles to db and show in view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
 class HomeController < ApplicationController
   def index
     @msg = 'hello!'
+    @msg2 = 'hoge'
+    @posts = Post.all.order('date DESC')
+
+    postdate = '20170712000001'
+    flag = 1
+    Post.all.each do |p|
+      if p.date == postdate
+        @msg = 'kuku'
+        if p.hp == 'kyodai'
+          @msg = 'haha'
+          flag = 0
+          break
+        end
+      end
+    end
+    if flag == 1
+      Post.create(hp: 'kyodai', title: 'hogehoge', author: 'yyamada', date: postdate, article: 'hugehuge\nhogehoge' )
+      @msg = 'mumu'
+    end
 
     require 'open-uri'
     require 'nokogiri'
@@ -43,5 +62,61 @@ class HomeController < ApplicationController
       articles.push(article.text)
     end
     @articles = articles
+
+    titles.size.times do |i|
+      flag = 1
+      Post.all.each do |p|
+        if p.date == convert_date(createds[i])
+          if p.hp == doc.title
+            flag = 0
+            break
+          end
+        end
+      end
+      if flag == 1
+        Post.create(hp: doc.title, title: titles[i], author: authors[i], date: convert_date(createds[i]), article: articles[i] )
+      end
+    end
+
   end
+
+  def convert_date(text)
+    y_i = text.index('年')
+    y = text.slice((y_i-4)..(y_i-1))
+
+    m_i = text.index('月')
+    m = text.slice((m_i-2)..(m_i-1))
+    if m.to_i.to_s != m
+      m[0] = '0'
+    end
+    m
+
+    d_i = text.index('日(')
+    d = text.slice((d_i-2)..(d_i-1))
+    if d.to_i.to_s != d
+      d[0] = '0'
+    end
+    d
+
+    h_i = text.index('時')
+    h = text.slice((h_i-2)..(h_i-1))
+    if h.to_i.to_s != h
+      h[0] = '0'
+    end
+
+    mi_i = text.index('分')
+    mi = text.slice((mi_i-2)..(mi_i-1))
+    if mi.to_i.to_s != mi
+      mi[0] = '0'
+    end
+
+    s_i = text.index('秒')
+    s = text.slice((s_i-2)..(s_i-1))
+    if s.to_i.to_s != s
+      s[0] = '0'
+    end
+    ret = y + m + d + h + mi + s
+    return ret
+  end
+
 end
---
+++ db/migrate/20171007065807_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :hp
+      t.string :title
+      t.string :author
+      t.datetime :date
+      t.text :article
+
+      t.timestamps
+    end
+  end
+end
